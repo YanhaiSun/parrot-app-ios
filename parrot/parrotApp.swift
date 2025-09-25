e8e69f45ed0c38ee9ea7ed01f48eb58461c5b94a
sunyanhai <guckask@gmail.com>
2025-09-26 01:51:05 +0800

    Initial Commit

---
+++ parrot/parrotApp.swift
@@ -0,0 +1,17 @@
+//
+//  parrotApp.swift
+//  parrot
+//
+//  Created by sunyanhai on 2025/9/26.
+//
+
+import SwiftUI
+
+@main
+struct parrotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
